2d992415050254d9cad3dbdb603f54f222c95f60
Dipanshu Negi <Dipanshu.negi@radcom.com>
2022-08-17 13:59:18 +0530

    updated code

--- src/main/java/com/schedulerTest/dipanshu/Controller/RequestController.java
+++ src/main/java/com/schedulerTest/dipanshu/Controller/RequestController.java
@@ -5,6 +5,9 @@ import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// in main file 
+
 @RestController
 public class RequestController {
 
